a894d5e6e5c6467ed0fcaab6fde962c32142401b
tzebohn <tzebohnling@gmail.com>
2025-09-18 18:11:44 -0400

    Created TabPanel component
    Can handle multi-form navigation via tabs.
    Personal, SkillSet, Education, and Experience tabs.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import { Header } from "./components/Header";
+import { TabPanel } from "./components/TabPanel";
 function App () {
   return (
     <div className="app">
       <Header />
+
+      <main className="flex flex-col md:flex-row md:align-center h-full bg-gray-100 p-5">
+        <section className="w-full border rounded-md bg-white">
+          <TabPanel />
+          <div className=""></div>
+        </section>
+        <section className="w-full border bg-white">
+          preview template
+        </section>
+      </main>
     </div>
   )
 }
---
+++ src/components/TabPanel.jsx
@@ -0,0 +1,56 @@
+import { IoMdPerson } from "react-icons/io";
+import { FaStar } from "react-icons/fa6";
+import { FaGraduationCap } from "react-icons/fa6";
+import { BiSolidBriefcaseAlt2 } from "react-icons/bi";
+import { useState } from "react";
+export function TabPanel () {
+
+    //UseState hooks
+    const [tab, setTab] = useState("personalTab") // Tab useState
+
+    //Tailwindcss styling
+    let tabStyle = `flex justify-center items-center flex-1 sm:gap-2 cursor-pointer`
+    let iconStyle = `text-sm sm:text-md md:text-xl lg:text-2xl`
+    let spanStyle = `text-sm sm:text-md md:text-lg lg:text-xl`
+
+    //Function updates Tab useState
+    const changeTab = (tabName) => {
+        if (tab !== tabName) {
+            //console.log(`Current tab: ${tab} Changing to: ${tabName}`)
+            setTab(tabName)
+        }
+    } 
+
+    return (
+        <div className="flex">
+            <div className="w-full">
+                <div className={`${tabStyle}`} onClick={() => changeTab("personalTab")}>
+                    <IoMdPerson className={`${iconStyle} text-blue-900`}/> 
+                    <span className={`${spanStyle}`}>Personal</span>
+                </div>
+                {tab === "personalTab" && <div className="border border-blue-500"></div>}
+            </div>
+            <div className="w-full">
+                <div className={`${tabStyle}`} onClick={() => changeTab("skillsetTab")}>
+                    <FaStar className={`${iconStyle} text-amber-500`}/> 
+                    <span className={`${spanStyle}`}>SkillSet</span>
+                </div>
+                {tab === "skillsetTab" && <div className="border border-blue-500"></div>}
+            </div>
+            <div className="w-full">
+                <div className={`${tabStyle}`} onClick={() => changeTab("educationTab")}>
+                    <FaGraduationCap className={`${iconStyle} text-blue-900`}/> 
+                    <span className={`${spanStyle}`}>Education</span>
+                </div>
+                {tab === "educationTab" && <div className="border border-blue-500"></div>}
+            </div>
+            <div className="w-full">
+                <div className={`${tabStyle}`} onClick={() => changeTab("experienceTab")}>
+                    <BiSolidBriefcaseAlt2 className={`${iconStyle} text-[#A0522D]`}/> 
+                    <span className={`${spanStyle}`}>Experience</span>
+                </div>
+                {tab === "experienceTab" && <div className="border border-blue-500"></div>}
+            </div>
+        </div>
+    )
+}
