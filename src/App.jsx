320b6ca803adbff72934d395102bdf51cc860868
tzebohn <tzebohnling@gmail.com>
2025-09-19 22:51:34 -0400

    Added userData state + minor styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
+import { useState } from "react";
 import { Header } from "./components/Header";
 import { TabPanel } from "./components/TabPanel";
 function App () {
+
+  //USESTATES HOOKS
+  const [userData, setUserData] = useState({
+    name: "",
+    email: "",
+    birthday: "",
+  });
+
   return (
-    <div className="app">
+    <div className="app w-full overflow-x-hidden">
       <Header />
 
-      <main className="flex flex-col md:flex-row md:align-center h-full bg-gray-100 p-5">
-        <section className="w-full border rounded-md bg-white">
-          <TabPanel />
-          <div className=""></div>
+      <main className="flex flex-col md:flex-row md:align-center min-h-screen bg-gray-100 p-5 min-w-0 md:justify-between gap-6">
+        <section className="w-full min-w-0 border rounded-md bg-white max-h-screen">
+          <TabPanel userData={userData} setUserData={setUserData} />
         </section>
-        <section className="w-full border bg-white">
-          preview template
+        <section className="w-full border rounded-md bg-white">
+          {JSON.stringify(userData)}
         </section>
       </main>
     </div>
